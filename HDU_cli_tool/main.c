f5aaa7b4a7fa87231bf3ab08de2a20e8140755bf
John Wu <524306184@qq.com>
2019-03-01 18:54:57 +0800

    初始化项目

---
+++ HDU_cli_tool/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HDU_cli_tool
+//
+//  Created by John Wu on 2019/3/1.
+//  Copyright © 2019 John Wu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
